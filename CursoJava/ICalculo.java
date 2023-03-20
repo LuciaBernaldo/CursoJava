f79124590530bf8884c9aba2cbe7038084a81d7e
LuciaBernaldo <128041674+LuciaBernaldo@users.noreply.github.com>
2023-03-20 11:46:51 +0100

    Delete ICalculo.java

--- CursoJava/ICalculo.java
+++
@@ -1,8 +0,0 @@
-package ejercicio2;
-
-public interface ICalculo {
-		public int sumar(int n1,int n2);
-		public int restar(int n1,int n2);
-		public int multiplicar(int n1,int n2);
-		public int dividir(int n1,int n2);
-}
